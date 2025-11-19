b73dadca17f51896c362e8b54c02d73d92b512a4
Valentina Nguyen <nguyen.valentina.99@gmail.com>
2025-11-19 14:38:35 +0100

    Delete chinese_chef.py

--- chinese_chef.py
+++
@@ -1,8 +0,0 @@
-from chef import Chef
-
-class ChineseChef(Chef): #inherit, you can overwrite it
-    
-    def make_special(self):
-        print("The chef makes an orange chicken")
-    def make_fried_rice(self):
-        print("The chef makes a fried rice")
